3060fc541903a9437b83ca95f599d929937bac9a
Noah Cameron <noah7cameron@gmail.com>
2018-04-19 12:59:50 -0400

    Updated Ship

--- NoConBattleShip/src/helverson/conall/Ship.java
+++ NoConBattleShip/src/helverson/conall/Ship.java
@@ -2,6 +2,35 @@ package helverson.conall;
 
 public class Ship {
 	
+	private String name;
 
+	private int size;
+
+	private boolean isHorizontal;
+
+	private boolean isPlaced;
+
+	private int x;
+
+	private int y;
+
+	private int shipLeft;
+
+	public Ship(String shipName, int shipSize) {
+		name = shipName;
+		size = shipSize;
+		isPlaced = false;
+		shipLeft = shipSize;
+
+	}
+
+	public void setLocation(int xValue, int yValue) {
+		x = xValue;
+		y = yValue;
+	}
+
+	public void damage() {
+		shipLeft -= 1;
+	}
 	
 }
